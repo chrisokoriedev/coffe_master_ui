adec5fe0377e0b496f811fc0a01341fb06466a62
christian-iuil <okoriec01@gmail.com>
2023-09-16 18:22:36 +0100

    created model and working of coffee list and data

--- lib/list.dart
+++ lib/list.dart
@@ -1,20 +1,34 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 import 'tab/coffee_tab.dart';
 
 final List<Tab> myTabs = <Tab>[
-    const Tab(text: 'Coffee'),
-    const Tab(text: 'Tea'),
-    const Tab(text: 'Pastries'),
-    const Tab(text: 'Sandwiches and Snack'),
-    const Tab(text: 'Cold Beverages'),
-  ];
+  const Tab(text: 'Coffee'),
+  const Tab(text: 'Tea'),
+  const Tab(text: 'Pastries'),
+  const Tab(text: 'Sandwiches and Snack'),
+  const Tab(text: 'Cold Beverages'),
+];
 
-  final List<Widget> tabViews = [
-    const CoffeeTab(),
-    const Center(child: Text('Tab 2 content')),
-    const Center(child: Text('Tab 2 content')),
-    const Center(child: Text('Tab 2 content')),
-    const Center(child: Text('Tab 2 content')),
-  ];
+final List<Widget> tabViews = [
+  const CoffeeTab(),
+  const Center(child: Text('Tab 2 content')),
+  const Center(child: Text('Tab 2 content')),
+  const Center(child: Text('Tab 2 content')),
+  const Center(child: Text('Tab 2 content')),
+];
 
+class ProductModel {
+  final String title;
+  final String subtitle;
+  final String imageUrl;
+  final double rating;
+  final String des;
+  final String price;
+  ProductModel(this.title, this.subtitle, this.imageUrl, this.rating, this.des,
+      this.price);
+}
+
+List<ProductModel> getCoffeeDetail = [
+  
+];
