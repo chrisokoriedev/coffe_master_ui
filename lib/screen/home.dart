7d816ca80d62c5db02e3a029ca54fa529e56ee60
christian-iuil <okoriec01@gmail.com>
2023-09-16 21:03:08 +0100

    making content animated

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,7 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:coffe_master_ui/import.dart';
 import 'package:flutter/material.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -10,7 +12,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-
+  int start = 200;
+  int delay = 200;
   @override
   void initState() {
     super.initState();
@@ -33,52 +36,70 @@ class _MyHomePageState extends State<MyHomePage>
           padding: const EdgeInsets.all(20),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(Icons.menu),
-                CircleAvatar(
-                  backgroundColor: lightBlueInput,
-                  child: FlutterLogo(),
-                ),
-              ],
+            BounceInLeft(
+              delay: Duration(milliseconds: delay),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.menu),
+                  CircleAvatar(
+                    backgroundColor: lightBlueInput,
+                    child: FlutterLogo(),
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 30),
-            const Text(
-              'Find the best \ncoffee ☕ for you',
-              style: TextStyle(
-                  fontSize: 28, fontWeight: FontWeight.bold, wordSpacing: 1.6),
+            BounceInUp(
+              delay: Duration(milliseconds: delay + start),
+              child: const Text(
+                'Find the best \ncoffee ☕ for you',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    wordSpacing: 1.6),
+              ),
             ),
             const SizedBox(height: 20),
-            TextFormField(
-              textAlignVertical: TextAlignVertical.center,
-              decoration: InputDecoration(
-                  filled: true,
-                  hintText: 'Find Your Coffee..',
-                  hintStyle: const TextStyle(
-                      fontSize: 12, color: Color.fromARGB(255, 125, 131, 145)),
-                  prefixIcon: const Icon(CupertinoIcons.search),
-                  isDense: true,
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  fillColor: lightBlueInput,
-                  border: inputborder(),
-                  enabledBorder: inputborder()),
+            FadeInDown(
+              delay: Duration(milliseconds: delay + 2 * start),
+              child: TextFormField(
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                    filled: true,
+                    hintText: 'Find Your Coffee..',
+                    hintStyle: const TextStyle(
+                        fontSize: 12,
+                        color: Color.fromARGB(255, 125, 131, 145)),
+                    prefixIcon: const Icon(CupertinoIcons.search),
+                    isDense: true,
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    fillColor: lightBlueInput,
+                    border: inputborder(),
+                    enabledBorder: inputborder()),
+              ),
             ),
             const SizedBox(height: 20),
-            TabBar(
-                controller: _tabController,
-                isScrollable: true,
-                dividerColor: Colors.transparent,
-                indicatorColor: brownColor,
-                unselectedLabelColor: tabText,
-                indicator: CircleTabIndicator(color: brownColor),
-                tabs: myTabs),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.2 + 120,
-              child: TabBarView(
-                controller: _tabController,
-                children: tabViews,
+            SlideInLeft(
+               delay: Duration(milliseconds: delay + 3 * start),
+              child: TabBar(
+                  controller: _tabController,
+                  isScrollable: true,
+                  dividerColor: Colors.transparent,
+                  indicatorColor: brownColor,
+                  unselectedLabelColor: tabText,
+                  indicator: CircleTabIndicator(color: brownColor),
+                  tabs: myTabs),
+            ),
+            SlideInRight(
+               delay: Duration(milliseconds: delay + 3 * start),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.2 + 120,
+                child: TabBarView(
+                  controller: _tabController,
+                  children: tabViews,
+                ),
               ),
             ),
             Text(
@@ -86,30 +107,41 @@ class _MyHomePageState extends State<MyHomePage>
               style: text.titleMedium,
             ),
             const SizedBox(height: 10),
-            Container(
-              height: 120,
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: lightBlueInput,
-                borderRadius: BorderRadius.circular(15),
-                gradient: linearPattern(),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                Expanded(
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30),
-                      child: Image.asset('$coffeeUrl/coffee_dash.jpeg')),
+            SlideInLeft(
+               delay: Duration(milliseconds: delay + 3 * start),
+
+              child: Container(
+                height: 120,
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: lightBlueInput,
+                  borderRadius: BorderRadius.circular(15),
+                  gradient: linearPattern(),
                 ),
-                Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('5 Coffee Beans You Must Try!',style: text.bodyLarge,)
-                  ],
-                )),
-              ]),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              '$coffeeUrl/coffee_dash.jpeg',
+                              fit: BoxFit.cover,
+                            )),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '5 Coffee Beans You Must Try!',
+                            style: text.bodyLarge,
+                          )
+                        ],
+                      )),
+                    ]),
+              ),
             )
           ]),
         ),
