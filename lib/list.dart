6ebd456b3d356bb28ef7c4f60df5f5d82d3de8ae
christian-iuil <okoriec01@gmail.com>
2023-09-24 18:49:14 +0100

    working on detail update blur and image

--- lib/list.dart
+++ lib/list.dart
@@ -28,49 +28,56 @@ class ProductModel {
   final String subtitle;
   final String imageUrl;
   final double rating;
-  final String des;
+  final String comment;
+  final String? description;
   final double price;
-  ProductModel(this.title, this.subtitle, this.imageUrl, this.rating, this.des,
-      this.price);
+  ProductModel(this.title, this.subtitle, this.imageUrl, this.rating,
+      this.comment, this.price, this.description);
 }
 
 List<ProductModel> getCoffeeDetail = [
-  ProductModel('Two Guns Espresso', 'With oat milk', '$coffeeUrl/coffee_a.jpeg',
-      4.5, 'it great', 3.99),
+  ProductModel(
+      'Two Guns Espresso',
+      'With oat milk',
+      '$coffeeUrl/coffee_a.jpeg',
+      4.5,
+      'it great',
+      3.99,
+      'Cortado coffee is a small, espresso-based drink that is popular in Spain and Portugal. It is made with equal parts espresso and steamed milk, and is typically served in a short glass. Cortado coffee has a rich, creamy flavor and is a great way to start your day.If you are looking for a coffee drink that is both delicious and satisfying, Cortado coffee is a great choice. It is also a good option for those who are looking for a coffee drink that is lower in caffeine than a traditional cup of coffee.'),
   ProductModel('Latte Coffee', 'With choco milk', '$coffeeUrl/coffee_b.jpeg',
-      4.5, 'it great', 3.99),
+      4.5, 'it great', 3.99, ''),
   ProductModel('Cortado Coffee', 'With normal milk', '$coffeeUrl/coffee_c.jpg',
-      4.5, 'it great', 3.99)
+      4.5, 'it great', 3.99, '')
 ];
 List<ProductModel> getTeaDetails = [
   ProductModel('Black Tea', 'With nothing', '$teaUrl/tea_a.png', 4.5,
-      'it great', 3.99),
+      'it great', 3.99, ''),
   ProductModel('Herbal Teae', 'With fresh leaf', '$teaUrl/tea_b.jpg', 4.5,
-      'it great', 3.99),
-  ProductModel('Rooibos Tea', '', '$teaUrl/tea_c.jpg', 4.5,
-      'it great', 3.99)
+      'it great', 3.99, ''),
+  ProductModel(
+      'Rooibos Tea', '', '$teaUrl/tea_c.jpg', 4.5, 'it great', 3.99, '')
 ];
 List<ProductModel> getPastriesDetails = [
   ProductModel('Two Guns Espresso', 'With nothing', '$coffeeUrl/coffee_a.jpeg',
-      4.5, 'it great', 3.99),
+      4.5, 'it great', 3.99, ''),
   ProductModel('Latte Coffee', 'With fresh leaf', '$coffeeUrl/coffee_b.jpeg',
-      4.5, 'it great', 3.99),
-  ProductModel('Cortado Coffee', '', '$coffeeUrl/coffee_c.jpg',
-      4.5, 'it great', 3.99)
+      4.5, 'it great', 3.99, ''),
+  ProductModel('Cortado Coffee', '', '$coffeeUrl/coffee_c.jpg', 4.5, 'it great',
+      3.99, '')
 ];
 List<ProductModel> getSandwichesAndSnackDetails = [
   ProductModel('Two Guns Espresso', 'With oat milk', '$coffeeUrl/coffee_a.jpeg',
-      4.5, 'it great', 3.99),
+      4.5, 'it great', 3.99, ''),
   ProductModel('Latte Coffee', 'With choco milk', '$coffeeUrl/coffee_b.jpeg',
-      4.5, 'it great', 3.99),
+      4.5, 'it great', 3.99, ''),
   ProductModel('Cortado Coffee', 'With normal milk', '$coffeeUrl/coffee_c.jpg',
-      4.5, 'it great', 3.99)
+      4.5, 'it great', 3.99, '')
 ];
 List<ProductModel> getColdBeverageDetails = [
   ProductModel('Two Guns Espresso', 'With oat milk', '$coffeeUrl/coffee_a.jpeg',
-      4.5, 'it great', 3.99),
+      4.5, 'it great', 3.99, ''),
   ProductModel('Latte Coffee', 'With choco milk', '$coffeeUrl/coffee_b.jpeg',
-      4.5, 'it great', 3.99),
+      4.5, 'it great', 3.99, ''),
   ProductModel('Cortado Coffee', 'With normal milk', '$coffeeUrl/coffee_c.jpg',
-      4.5, 'it great', 3.99)
+      4.5, 'it great', 3.99, '')
 ];
--- lib/screen/detail.dart
+++ lib/screen/detail.dart
@@ -9,10 +9,14 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final text = Theme.of(context).textTheme;
+    final _valueNotifier = ValueNotifier(false);
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(5.0),
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               width: double.infinity,
@@ -22,29 +26,200 @@ class DetailScreen extends StatelessWidget {
                   borderRadius: BorderRadius.circular(30),
                   image: DecorationImage(
                       image: AssetImage(data.imageUrl), fit: BoxFit.cover)),
-              child: ClipRRect(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5),
-                  child: Container(
-                    width: double.infinity,
-                    height: MediaQuery.of(context).size.height * 0.2,
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.4),
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20)),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15.0, vertical: 40),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                      Text(data.title),
-                      Text(data.subtitle),
-                    ]),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: menuBlue,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: const BackButton(
+                              style: ButtonStyle(
+                                  iconSize: MaterialStatePropertyAll(15),
+                                  tapTargetSize:
+                                      MaterialTapTargetSize.shrinkWrap,
+                                  fixedSize:
+                                      MaterialStatePropertyAll(Size(10, 10)))),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                              color: menuBlue,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: const Icon(
+                            CupertinoIcons.bolt_circle,
+                            size: 15,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30)),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5),
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(30),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.6),
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30)),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Flexible(
+                              flex: 7,
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      data.title,
+                                      style: text.titleLarge!.copyWith(
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                    Text(data.subtitle,
+                                        style: text.titleSmall!),
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.star,
+                                          color: brownColor,
+                                          size: 18,
+                                        ),
+                                        Text(data.rating.toString(),
+                                            style: text.titleMedium!),
+                                      ],
+                                    ),
+                                  ]),
+                            ),
+                            Flexible(
+                              flex: 4,
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Container(
+                                          width: 50,
+                                          height: 50,
+                                          decoration: BoxDecoration(
+                                              color: menuBlue,
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              const Icon(
+                                                Icons.coffee,
+                                                size: 18,
+                                                color: brownColor,
+                                              ),
+                                              Text('Coffee',
+                                                  style: text.bodySmall!
+                                                      .copyWith(height: 1.6)),
+                                            ],
+                                          ),
+                                        ),
+                                        Container(
+                                          width: 50,
+                                          height: 50,
+                                          decoration: BoxDecoration(
+                                              color: menuBlue,
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              const Icon(
+                                                Icons.water_drop,
+                                                size: 18,
+                                                color: brownColor,
+                                              ),
+                                              Text('Milk',
+                                                  style: text.bodySmall!
+                                                      .copyWith(height: 1.6)),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Container(
+                                      width: double.infinity,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                          color: menuBlue,
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text('Medium Frosted',
+                                              style: text.bodySmall!.copyWith(
+                                                  height: 1.6,
+                                                  color: Colors.white60)),
+                                        ],
+                                      ),
+                                    ),
+                                  ]),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Text(
+                    'Description',
+                    style: text.bodyMedium!.copyWith(color: Colors.white70),
+                  ),
+                  ValueListenableBuilder<bool>(
+                      valueListenable: _valueNotifier,
+                      builder: (context, value, child) {
+                        return Column(
+                          children: [
+                            Text(
+                              data.description!,
+                              maxLines: value ? null : 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                _valueNotifier.value = !_valueNotifier.value;
+                              },
+                              child: const Text('Read More'),
+                            ),
+                          ],
+                        );
+                      })
+                ],
+              ),
+            ),
           ],
         ),
       ),
